5e00a357b4c450d71ff8a38a8cfc5fae7d40d96b
Matt Elstone <matthew_elstone@hotmail.com>
2021-11-03 13:34:41 +0000

    added basic styling and about data

--- run.py
+++ run.py
@@ -9,8 +9,23 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/career")
+def career():
+    return render_template("career.html")
+
+
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP","0,0,0,0"),
+        host=os.environ.get("IP", "0,0,0,0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
